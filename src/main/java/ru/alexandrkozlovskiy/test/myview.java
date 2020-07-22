e77f866ab61bdc977c5ef96dbc71a991e81da4fd
AlexandrKozlovskiy <k.sasha1994@>
2020-07-22 18:00:08 +0300

    Рншена проблема с добавлением виртуального view

--- src/main/java/ru/alexandrkozlovskiy/test/myview.java
+++ src/main/java/ru/alexandrkozlovskiy/test/myview.java
@@ -6,15 +6,14 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityEventSource;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.view.accessibility.AccessibilityNodeProvider;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.annotation.Nullable;
-
 class myview extends TextView {
 private final myProvider p=new myProvider();
 private class myProvider extends AccessibilityNodeProvider {
@@ -23,49 +22,46 @@ private class myProvider extends AccessibilityNodeProvider {
     public AccessibilityNodeInfo createAccessibilityNodeInfo(int virtualViewId) {
         if(virtualViewId== View.NO_ID) {
             AccessibilityNodeInfo info = AccessibilityNodeInfo.obtain(myview.this);
-onInitializeAccessibilityNodeInfo(info);
-info.setParent((View) getParentForAccessibility());
-info.setVisibleToUser(true);
-            info.setImportantForAccessibility(true);
-info.addAction(AccessibilityNodeInfo.ACTION_CLICK);
-info.addAction(AccessibilityNodeInfo.ACTION_FOCUS);
-info.setSelected(true);
-info.setFocusable(true);
-info.setAccessibilityFocused(true);
-info.setContentDescription("Test of contentDescription of parent");
 info.addChild(myview.this,0);
-//Toast.makeText(getContext(),""+info,1).show();
+            info.addChild(myview.this,1);
+//announceForAccessibility(virtualViewId+" "+info);
+            onInitializeAccessibilityNodeInfo(info);
 return info;
         }
         else {
 AccessibilityNodeInfo info =AccessibilityNodeInfo.obtain();
-info.setContentDescription("Test of contentDescription");
 info.setSource(myview.this,virtualViewId);
 info.setParent(myview.this);
 info.setPackageName(getContext().getPackageName());
 info.setClassName(TextView.class.getName());
-            info.addAction(AccessibilityNodeInfo.ACTION_CLICK);
 int[] pos= {0,0};
 getLocationOnScreen(pos);
-info.setBoundsInScreen(new Rect(pos[0]+getLeft(),pos[1]+getTop(),pos[0]+getRight(),pos[1]+getBottom()));
+info.setBoundsInParent(new Rect(pos[0],pos[1],pos[0],pos[1]));
+info.setBoundsInScreen(new Rect(pos[0]+getLeft()*(virtualViewId+1),pos[1]+getTop()*(virtualViewId+1),pos[0]+getRight()*(virtualViewId+1),pos[1]+getBottom()*(virtualViewId+1)));
 info.setVisibleToUser(true);
 info.setImportantForAccessibility(true);
-info.setText("test of text");
 info.setEnabled(true);
 info.setFocusable(true);
 info.setFocused(true);
-            info.addAction(AccessibilityNodeInfo.ACTION_CLICK);
-            info.addAction(AccessibilityNodeInfo.ACTION_FOCUS);
-//Toast.makeText(getContext()," test"+info,1).show();
+info.addAction(AccessibilityNodeInfo.ACTION_CLICK);
+            info.setContentDescription("Test of contentDescription "+pos[0]+" "+pos[1]);
+//announceForAccessibility(virtualViewId+" "+info);
 return info;
 }
 }
 
     @Override
     public boolean performAction(int virtualViewId, int action, Bundle arguments) {
-if(action==AccessibilityNodeInfo.ACTION_CLICK) Toast.makeText(getContext(),virtualViewId+"",1).show();
-return super.performAction(virtualViewId, action, arguments);
-    }
+if(action==AccessibilityNodeInfo.ACTION_CLICK ||action==AccessibilityNodeInfo.ACTION_ACCESSIBILITY_FOCUS) {
+    AccessibilityEvent event = AccessibilityEvent.obtain(action == AccessibilityNodeInfo.ACTION_ACCESSIBILITY_FOCUS ? AccessibilityEvent.TYPE_VIEW_ACCESSIBILITY_FOCUSED : AccessibilityEvent.TYPE_VIEW_CLICKED);
+    if (action == AccessibilityNodeInfo.ACTION_CLICK) announceForAccessibility(virtualViewId + "");
+    event.setPackageName(getContext().getPackageName());
+    event.setSource(myview.this, virtualViewId);
+if(action==AccessibilityNodeInfo.ACTION_ACCESSIBILITY_FOCUS) getParent().requestSendAccessibilityEvent(myview.this, event);
+    return action == AccessibilityNodeInfo.ACTION_ACCESSIBILITY_FOCUS ? true : false;
+}
+return super.performAction(virtualViewId,action,arguments);
+}
 }
 public myview(Context context) {
         super(context);
@@ -78,9 +74,4 @@ super(context, attrs);
 return p;
 }
 
-    @Override
-    public void onInitializeAccessibilityNodeInfo(AccessibilityNodeInfo info) {
-        super.onInitializeAccessibilityNodeInfo(info);
-//Toast.makeText(getContext(),""+info,1).show();
-}
 }
